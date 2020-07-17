e84edd76adb0b7de6ac349bf9328ea871af147e5
Fabio Silva Ferreira <fabiosvf@gmail.com>
2020-07-17 00:51:04 -0300

    Nível 01 - Front-end com ReactJS - Estado e Imutabilidade - Varrendo uma lista com map

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ import Header from "./components/Header";
 /**
  * Componente
  * Propriedade
- * Estado
+ * Estado & Imutabilidade
  */
 
 function App() {
+  const projects = ["Desenvolvimento de app", "Front-end web"];
+
+  function handleAddProject() {
+    projects.push(`Novo projeto ${Date.now()}`);
+
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>HomePage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 }
